bbc7d0adac812b5d65c617bdbda283e70aaccda5
tweasted <78971195+tweasted@users.noreply.github.com>
2023-03-17 16:33:42 +0100

    Test Const

--- Projet IA 2/Projet IA 2/Program.cs
+++ Projet IA 2/Projet IA 2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+
+    static void geneSequence(string sequence, string motif)
+    {
+        int count = 0;
+        int index = sequence.IndexOf(motif);
+
+        while (index >= 0)
+        {
+            count++;
+            index = sequence.IndexOf(motif, index + 1);
+        }
+
+        Console.WriteLine("Le motif \"{0}\" apparaît {1} fois dans la séquence \"{2}\"", motif, count, sequence);
+    }
+
+    static void Main()
+    {
+        Console.Write("Entrez une séquence de gènes: ");
+        string sequence = Console.ReadLine();
+
+        Console.Write("Entrez un motif à rechercher: ");
+        string motif = Console.ReadLine();
+
+        geneSequence(sequence, motif);
+    }
+}
