8c7615ef0bb2b45ed16d77b3303331e7360c3565
4thenax <constance.blery@edu.ece.fr>
2023-03-17 16:51:56 +0100

    test1

--- Projet IA 2/Projet IA 2/Program.cs
+++ Projet IA 2/Projet IA 2/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 class Program
 {
-
+  
     static void geneSequence(string sequence, string motif)
     {
         int count = 0;
         int index = sequence.IndexOf(motif);
 
+        // Calcule le nombre d'occurrences du motif dans la séquence de gènes et affiche le résultat à la console
         while (index >= 0)
         {
             count++;
@@ -16,13 +17,34 @@ class Program
         Console.WriteLine("Le motif \"{0}\" apparaît {1} fois dans la séquence \"{2}\"", motif, count, sequence);
     }
 
+
     static void Main()
     {
+        // demande à l'utilisateur d'entrer une séquence de gène
         Console.Write("Entrez une séquence de gènes: ");
         string sequence = Console.ReadLine();
 
+        // Vérifie si la chaîne entrée est nulle ou vide
+        while (string.IsNullOrEmpty(sequence))
+        {
+            Console.WriteLine("Veuillez entrer une chaîne de caractères valide.");
+            sequence = Console.ReadLine();
+            
+        }
+
+        //demande à l'utilisateur d'entrer un motif à rechercher dans cette séquence
         Console.Write("Entrez un motif à rechercher: ");
         string motif = Console.ReadLine();
+
+
+        // Vérifie si la chaîne entrée est nulle ou vide
+        while (string.IsNullOrEmpty(motif))
+        {
+            Console.WriteLine("Veuillez entrer une chaîne de caractères valide.");
+            motif = Console.ReadLine();
+            
+        }
+
 
         geneSequence(sequence, motif);
     }
